6fa9a7964ba7afab17afdba10bdacc6fda461bc7
bhaa358 <bhaafaris1@gmail.com>
2025-02-03 17:45:52 +0200

    Update signin.php

--- signin.php
+++ signin.php
@@ -60,6 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['role'] = $user['Host'] ? 'admin' : 'guest'; 
         $_SESSION['userId'] = $user['id']; // Store user ID
         $_SESSION['userName'] = $user['fname']; // Optionally store user first name
+        $_SESSION['email'] = $user['email'];
     
         // Redirect based on existing behavior
         header("Location: toursss.php");
